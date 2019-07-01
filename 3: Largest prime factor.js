b29f1367bdce9b5ce4ffa179056a29285826bc1a
mohamed17717 <root@localhost.localdomain>
2019-07-01 19:30:38 +0200

    modified proplem num 3 with its explaination

--- 3: Largest prime factor.js
+++ 3: Largest prime factor.js
@@ -1,4 +1,7 @@
-
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the given number?
+*/
 
 function largestPrimeFactor(number){
   for(let i = 2; i < Math.sqrt(number); i++){
