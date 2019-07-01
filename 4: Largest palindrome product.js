86a630a3ccad01067bf7ff7d61e5f6a1b59a0e33
mohamed17717 <root@localhost.localdomain>
2019-07-01 20:28:32 +0200

    modified proplem num 4 with its explaination

--- 4: Largest palindrome product.js
+++ 4: Largest palindrome product.js
@@ -1,13 +1,38 @@
-digit  = n
-length = n*2
+/*
+A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
-'9  - -- -  9'
+Find the largest palindrome made from the product of two n-digit numbers.
+*/
 
-'get palindrom'
-'parse int'
-'sort'
-'reverse'
+function isPalindrome(s){
+  s = String(s);
+  return s === s.split('').reverse().join('')
+}
 
-'loop throw int(str(9)*n) -- as i'
-  'divide by i if length !== n: continue'
-  'check if division is int then i reached'
+function maxNumWith_nDigits(digits){
+  return parseInt('9'.repeat(digits));
+}
+
+function isProductOf_nDigits_Nums(num, n) {
+  let max = maxNumWith_nDigits(n);
+  for(let i=max; i > 0; i--){
+    if(num % i === 0 && String(num / i).length === n)
+      return true
+  }
+  return false
+}
+
+function largestPalindromeProduct(n) {
+  let max = maxNumWith_nDigits(n);
+  let product = max * max;
+
+  let palindrome = product
+  while(!(isPalindrome(palindrome) && isProductOf_nDigits_Nums(palindrome, n))){
+    palindrome--
+  }
+  return palindrome
+}
+
+
+console.log(largestPalindromeProduct(2))
+console.log(largestPalindromeProduct(3))
