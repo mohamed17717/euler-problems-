2708675337687678180dc55eb78894c8bbf238fd
mohamed17717 <root@localhost.localdomain>
2019-07-01 19:46:02 +0200

    modified proplem num 7 with its explaination

--- 7: 10001st prime.js
+++ 7: 10001st prime.js
@@ -1,23 +1,29 @@
-function nthPrime(n){
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the nth prime number?
+*/
+
+function nthPrime(n) {
+  "use strict";
   let primes = [2];
   let primeFlag, num;
-  
-  num = 3;
+
+  num = primes[primes.length-1];
   while (primes.length < n) {
-    primeFlag = true;
-    for (let p of primes){
-      if (num%p === 0) {
-        primeFlag = false;
+    let isPrime = true
+    for (let p of primes) {
+      if (num % p === 0){
+        isPrime = false;
         break;
       }
     }
-    primeFlag? primes.push(num) : false;
+    if(isPrime)
+      primes.push(num)
     num++;
   }
-  return primes.pop();
+  return primes[n-1];
 }
 
 for(let i of [6,10,100,1000,10001] ){
   console.log(nthPrime(i));
-
 }
